d9e77cad7d8a4c2d59c3761cc33388a130d1a5b3
Christiano Castro <cgc.ecp19@uea.edu.br>
2024-03-12 13:13:33 -0400

    novo

---
+++ index.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Mr David's Pizzaria</title>
+    <meta charset="utf-8">
+    <meta name="author" content="Christiano Castro">
+    <meta name="generator" content="Atom e Sublime">
+    <meta name="keywords" content="HTML, CSS, JQuery">
+    <meta name="description" content="Site para pedidos em uma pizzaria">
+    <link rel="shortcut icon" href="C:\Users\chris\Desktop\Sistema-de-Pizzaria---Web-master\Sistema-de-Pizzaria---Web-master\projeto_html\imagens\david.png">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/estilo.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap" rel="stylesheet">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap');
+    </style>
+</head>
+
+
+<body>
+    <div class="container">
+        <header>
+            <!-- Removendo o texto "PizzaDelicia" -->
+        </header>
+
+        <div id="logo-container">
+            <a href="C:\Users\chris\Desktop\Sistema-de-Pizzaria---Web-master\Sistema-de-Pizzaria---Web-master\projeto_html\paginas\index(old).html"> <!-- Adicione o caminho desejado para a página inicial no atributo href -->
+                <img src="../imagens/david.png" alt="Logo Pizza Delicia">
+            </a>
+        </div>
+        
+        <nav>
+            <ul>
+                <li><a href="C:\Users\chris\Desktop\Sistema-de-Pizzaria---Web-master\Sistema-de-Pizzaria---Web-master\projeto_html\paginas\index(old).html">HOME</a></li>
+                <li><a href="C:\Users\chris\Desktop\Sistema-de-Pizzaria---Web-master\Sistema-de-Pizzaria---Web-master\projeto_html\paginas\sobre.html">SOBRE NÓS</a></li>
+                <li><a href="C:\Users\chris\Desktop\Sistema-de-Pizzaria---Web-master\Sistema-de-Pizzaria---Web-master\projeto_html\paginas\franquia.html">FRANQUIA</a></li>
+            </ul>
+            <div class="pedido-btn">
+                <a href="./pedido.html">FAÇA SEU PEDIDO</a>
+            </div>
+
+            <div class="contato-btn">
+                <a href="./pedido.html">Entre em contato</a>
+            </div>
+        </nav>
+        
+
+
+        <br><br><br><br><br>
+        <div id="Peca">
+            <p>Peça Online</p>
+        </div>
+
+        <div id="Promocoes">
+            <p>Promoções<br> em Destaque</p>
+        </div>
+        
+        <div class="yellow-line">
+
+        </div>
+
+		<br><?php echo "Testando PHP:"?><br>
+		<br><?php echo "O formulário gerado" . " às " . date('h:i:s'); ?><br>
+        
+        <br><br><br><br>
+
+        <div id="image-container">
+            <a href="C:\Users\chris\Desktop\Sistema-de-Pizzaria---Web-master\Sistema-de-Pizzaria---Web-master\projeto_html\paginas\pedido.html"> <!-- Adicione o caminho desejado para a página inicial no atributo href -->
+            
+            <img src="C:\Users\chris\Desktop\Sistema-de-Pizzaria---Web-master\Sistema-de-Pizzaria---Web-master\projeto_html\imagens\1.jpg" alt="1">
+        </div>
+
+        <div id="image-container1-1">
+            <a href="C:\Users\chris\Desktop\Sistema-de-Pizzaria---Web-master\Sistema-de-Pizzaria---Web-master\projeto_html\paginas\pedido.html"> <!-- Adicione o caminho desejado para a página inicial no atributo href -->
+            
+            <img src="C:\Users\chris\Desktop\Sistema-de-Pizzaria---Web-master\Sistema-de-Pizzaria---Web-master\projeto_html\imagens\2.jpg" alt="2">
+        </div>
+
+        <div id="image-container1-2">
+            <a href="C:\Users\chris\Desktop\Sistema-de-Pizzaria---Web-master\Sistema-de-Pizzaria---Web-master\projeto_html\paginas\pedido.html"> <!-- Adicione o caminho desejado para a página inicial no atributo href -->
+            
+            <img src="C:\Users\chris\Desktop\Sistema-de-Pizzaria---Web-master\Sistema-de-Pizzaria---Web-master\projeto_html\imagens\3.jpg" alt="3">
+        </div>
+
+        <div id="image-container1-3">
+            <a href="C:\Users\chris\Desktop\Sistema-de-Pizzaria---Web-master\Sistema-de-Pizzaria---Web-master\projeto_html\paginas\pedido.html"> <!-- Adicione o caminho desejado para a página inicial no atributo href -->
+            
+            <img src="C:\Users\chris\Desktop\Sistema-de-Pizzaria---Web-master\Sistema-de-Pizzaria---Web-master\projeto_html\imagens\4.jpg" alt="4">
+        </div>
+        
+
+        <div id="image-container2">
+            <img src="C:\Users\chris\Desktop\Sistema-de-Pizzaria---Web-master\Sistema-de-Pizzaria---Web-master\projeto_html\imagens\pizza_salgada-removebg-preview.png" alt="Pizza Salgada">
+    
+            <img src="C:\Users\chris\Desktop\Sistema-de-Pizzaria---Web-master\Sistema-de-Pizzaria---Web-master\projeto_html\imagens\pizza_doce-removebg-preview.png" alt="Pizza Doce">
+    
+            <img src="C:\Users\chris\Desktop\Sistema-de-Pizzaria---Web-master\Sistema-de-Pizzaria---Web-master\projeto_html\imagens\pizza_dois_sabores-removebg-preview.png" alt="Pizza Dois Sabores">
+    
+            <img src="C:\Users\chris\Desktop\Sistema-de-Pizzaria---Web-master\Sistema-de-Pizzaria---Web-master\projeto_html\imagens\pizza_frango-removebg-preview.png" alt="Pizza de Frango">
+        </div>
+
+        <div id="Torne">
+            <p>Torne - se um franqueado</p>
+        </div>
+
+        <div id="Pizza-Salgada">
+            <p>Pizza Salgada</p>
+        </div>
+
+        <div id="Pizza-Doce">
+            <p>Pizza Doce</p>
+        </div>
+        
+        <div id="Pizza-Dois">
+            <p>Pizza Dois Sabores</p>
+        </div>
+
+        <div id="Pizza-Monte">
+            <p>Monte Sua Pizza</p>
+        </div>
+
+        <br>
+        <footer>
+        </footer>
+        <br>
+
+        <div class="kaushan-script-regular">
+            <p>Mr David's Pizzaria & Delivery</p>
+        </div>
+
+        <div class="kaushan-script-regular1">
+            <p>A melhor pizza da Cidade</p>
+        </div>
+
+        <div class="linha-vertical"></div>
+
+        <div id="HOMEE">
+            <a href="C:\Users\chris\Desktop\Sistema-de-Pizzaria---Web-master\Sistema-de-Pizzaria---Web-master\projeto_html\paginas\index(old).html"> <!-- Adicione o caminho desejado para a página inicial no atributo href -->
+
+            <p>Home</p>
+
+            <div id="SOBREE">
+                <a href="C:\Users\chris\Desktop\Sistema-de-Pizzaria---Web-master\Sistema-de-Pizzaria---Web-master\projeto_html\paginas\sobre.html"> <!-- Adicione o caminho desejado para a página inicial no atributo href -->
+    
+                <p>Sobre Nós</p>
+
+            <div id="FRANQUIAA">
+                <a href="C:\Users\chris\Desktop\Sistema-de-Pizzaria---Web-master\Sistema-de-Pizzaria---Web-master\projeto_html\paginas\franquia.html"> <!-- Adicione o caminho desejado para a página inicial no atributo href -->
+        
+                <p>Franquia </p>
+
+            <div id="TERMOS">
+                <a href="C:\Users\chris\Desktop\Sistema-de-Pizzaria---Web-master\Sistema-de-Pizzaria---Web-master\projeto_html\paginas\termos.html"> <!-- Adicione o caminho desejado para a página inicial no atributo href -->
+            
+                <p>Termos De Uso  </p>
+
+            <div id="POLITICA">
+                <a href="C:\Users\chris\Desktop\Sistema-de-Pizzaria---Web-master\Sistema-de-Pizzaria---Web-master\projeto_html\paginas\politica.html"> <!-- Adicione o caminho desejado para a página inicial no atributo href -->
+                
+                <p>Politica De Privacidade   </p>
+        </div>
+
+
+        <br>
+        
+        </nav>
+    </div>
+    <script type="text/javascript" src="../js/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript" src="../js/code.js"></script>
+    <script type="text/javascript" src="../js/bootstrap.min.js"></script>
+</body>
+
+</html>
